935f31d09e09375c43b5a9a781861d255b525f20
MaDHouSe79 <madhouse1979@gmail.com>
2023-09-04 13:58:26 +0200

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,8 +1,8 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 RegisterNetEvent('mh-cashasitem:server:update', function(type, amount, set, reason)
-	local src = source
-	local Player = QBCore.Functions.GetPlayer(src)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
     if Player and tostring(type) == 'cash' then
         if tostring(set) == "add" then Player.Functions.AddItem(Config.MoneyItem, amount, reason) end
         if tostring(set) == "remove" then Player.Functions.RemoveItem(Config.MoneyItem, amount, reason) end
