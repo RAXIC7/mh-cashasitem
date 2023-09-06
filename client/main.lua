a377aa36e8f2e674d630b6cc15e1f48ea2be7d45
MaDHouSe79 <madhouse1979@gmail.com>
2023-09-06 09:35:32 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,3 +1,6 @@
+--[[ ===================================================== ]]--
+--[[           MH Cash As Item Script by MaDHouSe          ]]--
+--[[ ===================================================== ]]--
 
 RegisterNetEvent("QBCore:Client:OnMoneyChange", function(type, amount, set, reason)
     TriggerServerEvent('mh-cashasitem:server:update', type, amount, set, reason)
